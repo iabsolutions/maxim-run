3d15c3e3cc74548b1d4c1c61aa42a742f8bf2249
Matt Addis <matthew.addis@gmail.com>
2024-10-23 23:47:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,41 @@ async function run() {
   try {
     // Get API key input
     const apiKey = core.getInput('api-key');
+    if (!apiKey) {
+      throw new Error('API key is required');
+    }
 
-    // // Make a request to Maxim Cloud API
-    // const response = await axios.post('https://www.maximcloud.io/api/v1/run-test', null, {
-    //   headers: {
-    //     'Authorization': `Bearer ${apiKey}`
-    //   }
-    // });
+    // Check for 'repository' input
+    const repository = core.getInput('repository');
+    if (!repository) {
+      throw new Error('Repository is required');
+    }
 
-    // // Log and output the result
-    // console.log(`Test triggered successfully: ${response.data}`);
-    // core.setOutput('result', response.data);
+    // Check for 'branch' input and default to 'main' if not provided
+    const branch = core.getInput('branch');
+    if (!branch) {
+      core.info('Branch not provided, defaulting to main');
+      branch = 'main';
+    }
 
-    // Mock output and send a test ID for consumption by other actions
-    console.log('Test triggered successfully: 123456789');
-    core.setOutput('result', '123456789');
+    // Check for 'token' input and default to null if not provided
+    const token = core.getInput('token');
+    if (!token) {
+      core.info('Token not provided, defaulting to null');
+      token = null;
+    }
+
+    // Make a request to Maxim Cloud API
+    const response = await axios.post('https://www.maximcloud.io/api/run-test?api_key=' + apiKey, 
+    { 
+        repository: repository,
+        branch: branch,
+        token: token
+    });
+
+    // Log and output the result
+    console.log(`Test triggered successfully: ${response.data}`);
+    core.setOutput('result', response.data);
 
     // Exit with success
     core.setOutput('success', true);
