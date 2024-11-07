e706018ed4945adcaaa359e59ec321d70223e71b
Matt Addis <matthew.addis@gmail.com>
2024-11-07 12:36:59 +0000

    Added ngrok

--- dist/index.js
+++ dist/index.js
@@ -30584,39 +30584,41 @@ const axios = __nccwpck_require__(6545);
 
 async function run() {
   try {
+    // Base URL for the API
+    let runUrl = "https://www.maximcloud.io/api/run-test";
 
-    // Run URL
-    const runUrl = "https://www.maximcloud.io/api/run-test"
+    // Post body object
+    const body = {};
 
-    // Post body
-    var body = {};
-
-    // Convert all input variables into a JSON object
-    for (const [key, value] of Object.entries(core.getInputMap())) {
-      body[key] = value;
+    // Retrieve all known inputs and add them to the body
+    const inputs = ['repository', 'branch', 'pat', 'ngrok-url', 'api-key'];
+    for (const inputName of inputs) {
+      const value = core.getInput(inputName);
+      if (value) {
+        body[inputName] = value;
+      }
     }
 
-    // Get API key input
+    // Get the API key
     const apiKey = core.getInput('api-key');
     if (!apiKey) {
       throw new Error('API key is required');
     }
 
-    // Check if ngrok URUL is provided
+    // Check if ngrok URL is provided and override the default URL
     const ngrokUrl = core.getInput('ngrok-url');
     if (ngrokUrl) {
       runUrl = ngrokUrl;
     }
 
-    // Make a request to Maxim Cloud API
-    const response = await axios.post(runUrl + '?api_key=' + apiKey, body);
+    // Make a POST request to Maxim Cloud API
+    const response = await axios.post(`${runUrl}?api_key=${apiKey}`, body);
 
     // Check for success response
     if (response.status !== 200) {
       throw new Error(`Request failed with status code: ${response.status}`);
     }
 
-    // Check for success response
     if (response.data !== 'success') {
       throw new Error(`Request failed with message: ${response.data}`);
     }
@@ -30633,6 +30635,7 @@ async function run() {
 }
 
 run();
+
 })();
 
 module.exports = __webpack_exports__;
